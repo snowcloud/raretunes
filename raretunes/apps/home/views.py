d08ed4450ef8313392a3cf31c8632c74b15577de
snowcloud <derek@snowcloud.co.uk>
2010-12-29 19:43:47 +0000

    added sitetemplates and site home

--- raretunes/apps/home/views.py
+++ raretunes/apps/home/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+""" views for home app
+
+"""
+
+from django.shortcuts import render_to_response
+
+
+def index(request):
+    return render_to_response('home/index.html')
--- raretunes/settings.py
+++ raretunes/settings.py
@@ -27,17 +27,18 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/London'
+DATE_FORMAT='%d %B %Y, %H:%M'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale
@@ -45,20 +46,31 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+# MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+# MEDIA_URL = ''
+
+import os
+import sys
+PROJECT_PATH = os.path.abspath(os.path.split(__file__)[0])
+
+sys.path.insert(0, os.path.join(PROJECT_PATH, "apps"))
+sys.path.insert(0, os.path.join(PROJECT_PATH, "libs"))
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'static')
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_PATH, 'sitetemplates')
+)
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/admin/media/'
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '-*rl_*v3!p5!7h41+%$ztj6+q=x05wr9wb%dpzhet@+22$4_*%'
+# Make this unique, put your own key here or override in settings_local.py
+SECRET_KEY = 'ju7vWreJndmIshuN5mmFjTYgeRFHCyuMpWwYXOrzJrVwwXvx'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -73,26 +85,28 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    "pagination.middleware.PaginationMiddleware",
+    "django.contrib.redirects.middleware.RedirectFallbackMiddleware",
+    "django.contrib.flatpages.middleware.FlatpageFallbackMiddleware",
 )
 
 ROOT_URLCONF = 'raretunes.urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
 INSTALLED_APPS = (
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.flatpages',
+    'django.contrib.humanize',
+    'django.contrib.markup',
+    'django.contrib.redirects',
+    'contact_form',
+    'pagination',
+    'home',
+    
 )
 
 # override any of the above in your own settings_local.py
