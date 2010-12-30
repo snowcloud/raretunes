b825082a9f0fb8db7cb9ffce63af9e99d4b0a580
snowcloud <derek@snowcloud.co.uk>
2010-12-30 21:53:57 +0000

    added TEMPLATE_CONTEXT_PROCESSORS to get request into generic views (paginator was failing)

--- raretunes/settings.py
+++ raretunes/settings.py
@@ -79,6 +79,15 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = ( 
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request"
+)
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -108,6 +117,7 @@ INSTALLED_APPS = (
     'tagging',
     'home',
     'recordings',
+    'scutils'
     
 )
 #  used in recordings.templatetags.recordingtags
