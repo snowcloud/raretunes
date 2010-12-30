0f5acbae999d1fb80eeef0b6681248a6d8139371
snowcloud <derek@snowcloud.co.uk>
2010-12-30 00:52:15 +0000

    work on basic styling

--- raretunes/settings.py
+++ raretunes/settings.py
@@ -1,6 +1,6 @@
 # Django settings for raretunes project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
--- raretunes/urls.py
+++ raretunes/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
-
 from django.contrib import admin
+
+from os import path as os_path
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -13,3 +16,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(.*)$', 'django.views.static.serve', {'document_root': os_path.join(settings.PROJECT_PATH, 'static')}),
+    )
