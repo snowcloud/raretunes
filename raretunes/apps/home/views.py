4440589ec60ee42c7442c4832bf4d20c4298e7bf
Derek Hoy <derek@snowcloud.co.uk>
2011-11-10 23:02:59 +0000

    redirect for rare.fm/

--- raretunes/apps/home/views.py
+++ raretunes/apps/home/views.py
@@ -2,8 +2,14 @@
 
 """
 
+from django.http import HttpResponseRedirect
+
 # from django.shortcuts import render_to_response
 # 
 # 
 # def index(request):
 #     return render_to_response('home/index.html')
+
+
+def rare_fm_home(request):
+    return HttpResponseRedirect('http://www.raretunes.org/')
--- raretunes/rarefm_urls.py
+++ raretunes/rarefm_urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
-    # url(r'^$', direct_to_template, { 'template': 'home/index.html' }, name='rarefm_home'),
+    url(r'^$', 'home.views.rare_fm_home', name='rarefm_home'),
     url(r'^r(?P<short_id>\w+)/$', 'recordings.views.short_recording_detail', name='short-recording'),
 
 )
